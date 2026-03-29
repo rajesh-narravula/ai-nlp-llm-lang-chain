352bfa4ce23717ffa46eeaf65d142d4663e1a216
Rajesh Narravula <rajesh.narravula@gmail.com>
2026-03-29 23:22:05 +0530

    langgraph codes

---
+++ lang-graph/langgraph_chat_bot_store_prev_question.py
@@ -0,0 +1,60 @@
+import config
+from langgraph.graph import START, END, StateGraph, add_messages
+from typing_extensions import TypedDict
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_core.messages import AIMessage, HumanMessage, BaseMessage
+from collections.abc import Sequence
+from typing import Literal, Annotated
+from pydantic import SecretStr
+
+class State(TypedDict):
+    message: Annotated[Sequence[BaseMessage], add_messages]
+
+chat = ChatOpenAI(model="gpt-4o-mini",
+                  temperature=0,
+                  api_key=SecretStr(config.api_key),
+                  max_completion_tokens=300)
+
+def ask_question(state: State) -> State:
+     print(f"\n-------> ENTERING Ask_question:")
+     question = "What is the question:"
+     print(question)
+     return State(message=[AIMessage(question), HumanMessage(input())])
+
+def chatbot(state: State) -> State:
+     print(f"\n-------> ENTERING chatbot:")
+     response = chat.invoke(state["message"])
+     response.pretty_print()
+     return State(message=[response])
+
+def ask_another_question(state: State) -> State:
+     print(f"\n-------> ENTERING Ask_another_question:")
+     question = "Do you have any other question(yes/no):"
+     print(question)
+     return State(message=[AIMessage(question), HumanMessage(input())])
+
+def routing_function(state: State) -> Literal["ask_question", "__end__"]:
+     if state["message"][-1].content == 'yes':
+        return "ask_question"
+     else:
+        return "__end__"
+
+
+graph_state = StateGraph(State)
+
+graph_state.add_node("ask_question", ask_question)
+graph_state.add_node("chatbot", chatbot)
+graph_state.add_node("ask_another_question", ask_another_question)
+
+graph_state.add_edge(START, "ask_question")
+graph_state.add_edge("ask_question", "chatbot")
+graph_state.add_edge("chatbot", "ask_another_question")
+graph_state.add_conditional_edges(source="ask_another_question", path=routing_function)
+
+graph_compiled = graph_state.compile()
+
+#print(graph_compiled.get_graph().draw_ascii())
+
+graph_compiled.invoke(State(message = []))
+
+
---
+++ lang-graph/run_config.md
@@ -0,0 +1,12 @@
+> python -m venv lang_graph_env
+
+> lang_graph_env\Scripts\activate
+
+> pip install langchain
+> pip install langgraph
+> pip install langchain-openai
+> pip install langgraph
+> pip install langgraph-checkpoint-sqlite
+> pip install mypy-ipython
+> pip install grandalf
+
